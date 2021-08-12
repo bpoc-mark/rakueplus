b2c28c82720cdc2da8a6cc0f0a197e6979762406
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-08-12 18:16:05 +0800

    sect_7 first commit

--- front-page.php
+++ front-page.php
@@ -18,7 +18,86 @@
         
     </section>
     <section class="sect_7">
-        
+        <div class="sect_7__wrapper">
+            <div class="l-wrap">
+
+                <!-- heading -->
+                <div class="heading">
+                    <p>よくある質問</p>
+                </div>
+
+                <!-- girl image -->
+                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_1.png" alt="" class="girl">
+
+                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_1_sp.png" alt="" class="girl_sp">
+
+                <!-- row -->
+                <div class="row">
+                
+                    <!-- column 1 -->
+                    <div class="col col_1">
+
+                        <div class="col_heading">
+                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2.png" alt="">
+                            <p>質問タイトルが入ります <br>
+                                質問タイトルが入ります</p>
+                        </div>
+
+                        <!-- sp heading -->
+                        <div class="col_heading_sp">
+                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2_sp.png" alt="">
+                            <p>質問タイトルが入ります <br>
+                                質問タイトルが入ります</p>
+                        </div>
+
+                       
+                        <p class="col_body">回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。</p>
+                        
+                    </div>
+
+                    <div class="col col_2">
+                        
+                        <div class="col_heading">
+                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2.png" alt="">
+                            <p>質問タイトルが入ります <br>
+                                質問タイトルが入ります</p>
+                        </div>
+
+                        <!-- sp heading -->
+                        <div class="col_heading_sp">
+                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2_sp.png" alt="">
+                            <p>質問タイトルが入ります <br>
+                                質問タイトルが入ります</p>
+                        </div>
+
+                        <p class="col_body">回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。</p>
+
+                    </div>
+   
+                    <div class="col col_3">
+                        
+                        <div class="col_heading">
+                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2.png" alt="">
+                            <p>質問タイトルが入ります <br>
+                                質問タイトルが入ります</p>
+                        </div>
+
+                        <!-- sp heading -->
+                        <div class="col_heading_sp">
+                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2_sp.png" alt="">
+                            <p>質問タイトルが入ります <br>
+                                質問タイトルが入ります</p>
+                        </div>
+
+                        <p class="col_body">回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。</p>
+
+                    </div>
+
+                </div>
+               
+
+            </div>
+        </div>
     </section>
     <section class="sect_8">
         
