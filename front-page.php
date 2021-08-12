7fef501c5031bf2a1abbdfa02828897db532b22c
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-08-12 13:53:52 +0800

    first commit sect_6

--- front-page.php
+++ front-page.php
@@ -15,11 +15,82 @@
         
     </section>
     <section class="sect_6">
-        <div class="sect_1__wrapper">
-            <div class="l_wrap">
-                
+        <div class="sect_6__wrapper">
+            <div class="l-wrap">
+                <!-- heading -->
                 <div class="heading">
                     <h1>RAKUE+の提供価値</h1>
+                </div>
+                <!-- row -->
+                <div class="row">
+                    <!-- column 1 -->
+                    <div class="col_1">
+                        <div class="col_heading">
+                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/img_1.png" alt="">
+                        </div>
+                    
+                        <div class="col_1__body">
+                            <div>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/icon_1.png" alt="">
+                                <p>営業マニュアルのお渡し</p>
+                            </div>
+                            <div>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/icon_1.png" alt="">
+                                <p>ロールプレイング</p>
+                            </div>
+                        </div>
+
+                        <p>説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。説明文が入る。説明文が入る。</p>
+                    </div>
+
+                    <!-- column 2 -->
+                    <div class="col_2">
+                        <div class="col_heading">
+                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/img_2.png" alt="">
+                        </div>
+
+                        <div class="col_2__body">
+                            <div>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/icon_1.png" alt="">
+                                <p>営業マニュアルのお渡し</p>
+                            </div>
+                            <div>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/icon_1.png" alt="">
+                                <p>ロールプレイング</p>
+                            </div>
+                            <div>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/icon_1.png" alt="">
+                                <p>営業同行 1回</p>
+                            </div>
+                        </div>
+
+                        <p>説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。説明文が入る。説明文が入る。</p>
+                    </div>
+
+                    <!-- column 3 -->
+                    <div class="col_3">
+                        <div class="col_heading">
+                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/img_3.png" alt="">
+                        </div>
+                        
+
+                        <div class="col_3__body">
+                            <div>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/icon_1.png" alt="">
+                                <p>営業マニュアルのお渡し</p>
+                            </div>
+                            <div>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/icon_1.png" alt="">
+                                <p>ロールプレイング</p>
+                            </div>
+                            <div>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_6/icon_1.png" alt="">
+                                <p>営業同行 1回</p>
+                            </div>
+                        </div>
+
+                        <p>説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。説明文が入る。説明文が入る。</p>
+                    </div>
                 </div>
 
             </div>
