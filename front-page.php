afcc7e00a56b96b0a2a33d762ea006f8882f8d89
arsua-greg <gregarsua@bpoc.co.jp>
2021-08-12 14:05:11 +0800

    sect_1

--- front-page.php
+++ front-page.php
@@ -1,26 +1,42 @@
-<?php get_header();?>
-    <section class="sect_1">
-         
-    </section>
-    <section class="sect_2">
-        
-    </section>
-    <section class="sect_3">
-        
-    </section>
-    <section class="sect_4">
-        
-    </section>
-    <section class="sect_5">
-        
-    </section>
-    <section class="sect_6">
-        
-    </section>
-    <section class="sect_7">
-        
-    </section>
-    <section class="sect_8">
-        
-    </section>
-<?php get_footer();?>
+<?php get_header(); ?>
+<section class="sect_1">
+    <div class="sect_1__wrapper">
+        <div class="l-wrap">
+            <div class="content">
+                <div class="content__col-1">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_1/img_1.png" alt="">
+                </div>
+
+                <div class="content__col-2">
+                    <div class="title_cont">
+                        <p class="title"><span>RAKUE＋</span>（ラクエプラス）</p>
+                        <div class="line"></div>
+                    </div>
+                    <p class="desc">説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。説明文が入る。説明文が入る。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sect_2">
+
+</section>
+<section class="sect_3">
+
+</section>
+<section class="sect_4">
+
+</section>
+<section class="sect_5">
+
+</section>
+<section class="sect_6">
+
+</section>
+<section class="sect_7">
+
+</section>
+<section class="sect_8">
+
+</section>
+<?php get_footer(); ?>
