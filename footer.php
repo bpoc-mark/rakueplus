02566d3edae7d92d920f4f7be4a72595a6428e27
bpoc-mark <markariel.maata@gmail.com>
2021-08-12 11:03:15 +0800

    added footer pc and sp view

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
         </div>
         <div class="page__footer">
-            
+            <p>© RAKUE-reform Produced by <a href="https://jeengross.jp/" target="_blank">jeegross</a></p>
         </div>
     </main>
 
