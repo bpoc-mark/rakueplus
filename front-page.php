d42c2e44cdd504b95e6fc8cc08b3c217ef20839e
bpoc-mark <markariel.maata@gmail.com>
2021-08-12 09:38:41 +0800

    rakueplus template

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php get_header();?>
+    <section class="sect_1">
+         
+    </section>
+    <section class="sect_2">
+        
+    </section>
+    <section class="sect_3">
+        
+    </section>
+    <section class="sect_4">
+        
+    </section>
+    <section class="sect_5">
+        
+    </section>
+    <section class="sect_6">
+        
+    </section>
+    <section class="sect_7">
+        
+    </section>
+    <section class="sect_8">
+        
+    </section>
+<?php get_footer();?>
