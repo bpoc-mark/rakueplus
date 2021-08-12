479a6acc4742b76f0bad4e0a0e01ee6fff007f11
bpoc-mark <markariel.maata@gmail.com>
2021-08-12 16:00:36 +0800

    added header pc and sp view

--- header.php
+++ header.php
@@ -17,6 +17,27 @@
     
 <main>
     <div class="page__header">
-        
+        <div class="header_container">
+            <div class="l-wrap">
+                <div class="head_cont">
+                    <div class="head_cont_1">
+                        <p class="p-t-75">RAKUE + for seles company service</p>
+                        <img src="<?php echo get_template_directory_uri();?>/release/image/header/head_cont_btm.png" alt="">
+                    </div>
+                    <div class="head_cont_2">
+                        <!-- <p>ラクエプラス</p> -->
+                        <img src="<?php echo get_template_directory_uri();?>/release/image/header/rakue_hero.png" alt="ラクエプラス RAKUE+">
+                        <p>
+                            <span>で導入もスムーズ</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="head_desc">
+                    <p>説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。説明文が入る。説明文が入る。</p>
+                </div>
+                <img class="rakue_img" src="<?php echo get_template_directory_uri();?>/release/image/header/rakue_image.png" alt="">
+                <img class="rakue_chair" src="<?php echo get_template_directory_uri();?>/release/image/header/rakue_chair.png" alt="">
+            </div>
+        </div>
     </div>
     <div class="page__content">
