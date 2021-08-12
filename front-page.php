ec1317eefff5f48743ed427d2545f7650d937559
arsua-greg <gregarsua@bpoc.co.jp>
2021-08-12 17:13:55 +0800

    sect_5

--- front-page.php
+++ front-page.php
@@ -1,26 +1,38 @@
-<?php get_header();?>
-    <section class="sect_1">
-         
-    </section>
-    <section class="sect_2">
-        
-    </section>
-    <section class="sect_3">
-        
-    </section>
-    <section class="sect_4">
-        
-    </section>
-    <section class="sect_5">
-        
-    </section>
-    <section class="sect_6">
-        
-    </section>
-    <section class="sect_7">
-        
-    </section>
-    <section class="sect_8">
-        
-    </section>
-<?php get_footer();?>
+<?php get_header(); ?>
+<section class="sect_1">
+
+</section>
+<section class="sect_2">
+
+</section>
+<section class="sect_3">
+
+</section>
+<section class="sect_4">
+
+</section>
+<section class="sect_5">
+    <div class="sect_5__wrapper">
+        <div class="l-wrap">
+            <div class="c-contact">
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/arrow_right.png" alt="Arrow Right">
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/rakue.png" alt="Rakue">
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/plus_sign.png" alt="Plus Sign">
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/txt.png" alt="Text">
+                <div class="woman">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/contact_woman.png" alt="Woman">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sect_6">
+
+</section>
+<section class="sect_7">
+
+</section>
+<section class="sect_8">
+
+</section>
+<?php get_footer(); ?>
