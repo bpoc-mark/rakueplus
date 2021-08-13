c18b75330f1843a6e41beb0103cee857499549bd
bpoc-mark <markariel.maata@gmail.com>
2021-08-13 16:13:04 +0800

    added section 8 pc and sp view

--- front-page.php
+++ front-page.php
@@ -21,6 +21,18 @@
         
     </section>
     <section class="sect_8">
-        
+        <div class="sect_8__wrapper">
+            <div class="l-wrap">
+                <h3 class="l-wrap__header m-b-60">お申し込み<br>フォーム</h3>
+                <p class="l-wrap__desc w-620 m-b-80">
+                    ・お問い合わせフォームに必要事項のご入力をして送信ボタンをクリックしてください。<br>
+                    ・メールアドレスは正しくご入力ください。弊社より返信メールが届かない可能性があります。<br>
+                    ・【必須】の項目は必ず入力してください。
+                </p>
+                <div class="form_container">
+                    <?php echo do_shortcode('[contact-form-7 id="26" title="Contact form 1"]'); ?>
+                </div>
+            </div>
+        </div>
     </section>
 <?php get_footer();?>
