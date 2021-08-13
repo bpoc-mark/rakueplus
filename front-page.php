b035d02f44a66d35559ccf81e68af86b368279e0
arsua-greg <gregarsua@bpoc.co.jp>
2021-08-13 14:35:19 +0800

    edit sect_5 same sa sect_2

--- front-page.php
+++ front-page.php
@@ -15,15 +15,24 @@
     <div class="sect_5__wrapper">
         <div class="l-wrap">
             <div class="c-contact">
-                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/arrow_right.png" alt="Arrow Right">
-                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/rakue.png" alt="Rakue">
-                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/plus_sign.png" alt="Plus Sign">
-                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/txt.png" alt="Text">
-                <div class="woman">
-                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_5/contact_woman.png" alt="Woman">
+                <div class="img_1">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_2/arrow_right.png" alt="Arrow Right">
+                </div>
+                <div class="img_2">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_2/rakue.png" alt="Rakue">
+                </div>
+                <div class="img_3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_2/plus_sign.png" alt="Plus Sign">
+                </div>
+                <div class="img_4">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_2/txt.png" alt="Text">
+                </div>
+                <div class="img_5">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_2/contact_woman.png" alt="Woman">
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </section>
 <section class="sect_6">
