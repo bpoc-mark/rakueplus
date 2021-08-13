946a06027fc1eebc711d9fc42f81b08f33314d5f
denver <denver.gomez@bpoc.co.jp>
2021-08-13 14:06:03 +0800

    implemented pc and sp

--- front-page.php
+++ front-page.php
@@ -1,26 +1,68 @@
 <?php get_header();?>
-    <section class="sect_1">
-         
-    </section>
-    <section class="sect_2">
-        
-    </section>
-    <section class="sect_3">
-        
-    </section>
-    <section class="sect_4">
-        
-    </section>
-    <section class="sect_5">
-        
-    </section>
-    <section class="sect_6">
-        
-    </section>
-    <section class="sect_7">
-        
-    </section>
-    <section class="sect_8">
-        
-    </section>
+<section class="sect_1">
+
+</section>
+<section class="sect_2">
+
+</section>
+<section class="sect_3">
+
+</section>
+<section class="sect_4">
+    <div class="l-wrap">
+        <div class="bg_overlay">
+        </div>
+        <div class="sect_4__heading-title title-primary">
+            <p><span>こんなお悩みありませんか?</span></p>
+        </div>
+        <div class="sect_4__cont_flex">
+            <div class="cont_desc mt-44">
+                <div class="desc mb-40">
+                    <img class="icon"
+                        src="<?php echo get_template_directory_uri(); ?>/release/image/sect_4/check_icon.png" alt="">
+                    <p class="txt_list">テキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
+                </div>
+                <div class="desc mb-40">
+                    <img class="icon"
+                        src="<?php echo get_template_directory_uri(); ?>/release/image/sect_4/check_icon.png" alt="">
+                    <p class="txt_list">テキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
+                </div>
+                <div class="desc mb-40">
+                    <img class="icon"
+                        src="<?php echo get_template_directory_uri(); ?>/release/image/sect_4/check_icon.png" alt="">
+                    <p class="txt_list">テキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
+                </div>
+                <div class="desc">
+                    <img class="icon"
+                        src="<?php echo get_template_directory_uri(); ?>/release/image/sect_4/check_icon.png" alt="">
+                    <p class="txt_list">テキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
+                </div>
+                <div class="txt_cont mt-65">
+                    <p class="txt">説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。</p>
+                    <p class="txt">説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。</p>
+                    <p class="txt">説明文が入る。説明文が入る。説明文が入る。説明文が入る。説明文が入る。 説明文が入る。</p>
+                </div>
+            </div>
+
+            <div class="cont_person">
+                <img class="person" src="<?php echo get_template_directory_uri(); ?>/release/image/sect_4/person_1.png"
+                    alt="">
+                <img class="mssg"
+                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_4/person_message.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sect_5">
+
+</section>
+<section class="sect_6">
+
+</section>
+<section class="sect_7">
+
+</section>
+<section class="sect_8">
+
+</section>
 <?php get_footer();?>
