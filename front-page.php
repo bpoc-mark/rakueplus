ac069f481bbcddee98767f3e5cedaa2e3f7fe2b0
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-08-16 10:30:55 +0800

    add position relative on wrapper and remove question background image and replace it using css

--- front-page.php
+++ front-page.php
@@ -38,17 +38,12 @@
                     <div class="col col_1">
 
                         <div class="col_heading">
-                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2.png" alt="Text">
                             <p>質問タイトルが入ります <br>
                                 質問タイトルが入ります</p>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/rectangle.png" alt="">
                         </div>
 
-                        <!-- sp heading -->
-                        <div class="col_heading_sp">
-                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2_sp.png" alt="Text">
-                            <p>質問タイトルが入ります <br>
-                                質問タイトルが入ります</p>
-                        </div>
+                     
 
                        
                         <p class="col_body">回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。</p>
@@ -58,17 +53,11 @@
                     <div class="col col_2">
                         
                         <div class="col_heading">
-                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2.png" alt="Text">
                             <p>質問タイトルが入ります <br>
                                 質問タイトルが入ります</p>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/rectangle.png" alt="">
                         </div>
 
-                        <!-- sp heading -->
-                        <div class="col_heading_sp">
-                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2_sp.png" alt="Text">
-                            <p>質問タイトルが入ります <br>
-                                質問タイトルが入ります</p>
-                        </div>
 
                         <p class="col_body">回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。</p>
 
@@ -77,17 +66,11 @@
                     <div class="col col_3">
                         
                         <div class="col_heading">
-                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2.png" alt="Text">
                             <p>質問タイトルが入ります <br>
                                 質問タイトルが入ります</p>
+                                <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/rectangle.png" alt="">
                         </div>
 
-                        <!-- sp heading -->
-                        <div class="col_heading_sp">
-                            <img src="<?= get_template_directory_uri(); ?>/release/image/sect_7/img_2_sp.png" alt="Text">
-                            <p>質問タイトルが入ります <br>
-                                質問タイトルが入ります</p>
-                        </div>
 
                         <p class="col_body">回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。回答テキストが入ります。</p>
 
